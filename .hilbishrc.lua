93ce2a9cef22f461cc1ae1a6be6da19dc0f1f114
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-03-20 18:51:51 -0400

    "feat: cd builtin at lua config"

--- .hilbishrc.lua
+++ .hilbishrc.lua
@@ -1,6 +1,21 @@
 -- Default Hilbish config
-package.path = package.path .. ';./libs/?/init.lua'
+package.path = package.path .. ';./libs/?/init.lua' .. ';/home/sammy/.luarocks/lib/lua/5.4/?.so'
+
+fs = require 'fs'
+commander = require 'commander'
+
+commander.register("cd", function (path)
+	fs.cd(path[1])
+end)
+--[[commander = {
+	__commands = {}
+}
+commander.__commands.ayo = function ()
+	print("ayo?")
+end]]--
 
 local ansikit = require 'ansikit'
 
 prompt(ansikit.text('λ {bold}{cyan}'..os.getenv('USER')..' >{magenta}>{cyan}>{reset} '))
+
+--hook("tab complete", function ())
