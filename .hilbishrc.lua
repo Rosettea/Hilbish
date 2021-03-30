cb6f17c8fa0b0cd578db71b62b671356ad380eee
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-03-30 19:48:37 -0400

    details: make triangle in prompt change color based on command result

--- .hilbishrc.lua
+++ .hilbishrc.lua
@@ -1,8 +1,21 @@
 -- Default Hilbish config
 ansikit = require 'ansikit'
+bait = require 'bait'
 
-prompt(ansikit.text(
-	'{blue}%u {cyan}%d {green}∆{reset} '
-))
+function doPrompt(fail)
+	prompt(ansikit.text(
+		'{blue}%u {cyan}%d ' .. (fail and '{red}' or '{green}') .. '∆{reset} '
+	))
+end
+
+doPrompt()
+
+bait.catch('command.fail', function()
+	doPrompt(true)
+end)
+
+bait.catch('command.success', function()
+	doPrompt()
+end)
 
 --hook("tab complete", function ())
