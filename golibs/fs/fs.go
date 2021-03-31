a9018a2740954c510109480dceea90055c552460
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-03-31 07:32:47 -0400

    fix: return correct error code and fix error level

--- golibs/fs/fs.go
+++ golibs/fs/fs.go
@@ -16,7 +16,7 @@ func Loader(L *lua.LState) int {
 
 func LuaErr(L *lua.LState, code int) {
 	// TODO: Error with a table, with path and error code
-	L.Error(lua.LNumber(code), 1)
+	L.Error(lua.LNumber(code), 2)
 }
 
 var exports = map[string]lua.LGFunction{
@@ -30,7 +30,7 @@ func cd(L *lua.LState) int {
 	if err != nil {
 		switch err.(*os.PathError).Err.Error() {
 		case "no such file or directory":
-			LuaErr(L, 2)
+			LuaErr(L, 1)
 		}
 	}
 
