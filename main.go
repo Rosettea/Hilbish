9cf03e37c4c5199e5de760497bba25367a10f65f
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-01-26 15:18:54 -0400

    fix: change hooks from signals to signal
    drop the s, dont remember when i added it
    it makes more sense and its what the docs have anyway

--- main.go
+++ main.go
@@ -265,17 +265,17 @@ func handleSignals() {
 	for s := range c {
 		switch s {
 		case os.Interrupt:
-			hooks.Em.Emit("signals.sigint")
+			hooks.Em.Emit("signal.sigint")
 			if !running && interactive {
 				lr.ClearInput()
 			}
 		case syscall.SIGWINCH:
-			hooks.Em.Emit("signals.resize")
+			hooks.Em.Emit("signal.resize")
 			if !running && interactive {
 				lr.Resize()
 			}
-		case syscall.SIGUSR1: hooks.Em.Emit("signals.sigusr1")
-		case syscall.SIGUSR2: hooks.Em.Emit("signals.sigusr2")
+		case syscall.SIGUSR1: hooks.Em.Emit("signal.sigusr1")
+		case syscall.SIGUSR2: hooks.Em.Emit("signal.sigusr2")
 		}
 	}
 }
