21b093559a6c911733b26b7e5cb81830475542d3
TorchedSammy <TorchedSammy@users.noreply.github.com>
2022-02-26 15:37:02 +0000

    docs: [ci] generate new docs

--- emmyLuaDocs/hilbish.lua
+++ emmyLuaDocs/hilbish.lua
@@ -3,10 +3,13 @@
 local hilbish = {}
 
 --- Sets an alias of `orig` to `cmd`
-function hilbish.alias() end
+--- @param cmd string
+--- @param orig string
+function hilbish.alias(cmd, orig) end
 
 --- Appends `dir` to $PATH
-function hilbish.appendPath() end
+--- @param dir string|table
+function hilbish.appendPath(dir) end
 
 --- Registers a completion handler for `scope`.
 --- A `scope` is currently only expected to be `command.<cmd>`,
@@ -19,16 +22,21 @@ function hilbish.complete() end
 function hilbish.cwd() end
 
 --- Replaces running hilbish with `cmd`
-function hilbish.exec() end
+--- @param cmd string
+function hilbish.exec(cmd) end
 
 --- Puts `fn` in a goroutine
-function hilbish.goroutine() end
+--- @param fn function
+function hilbish.goroutine(fn) end
 
 --- Runs the `cb` function every `time` milliseconds
-function hilbish.interval() end
+--- @param cb function
+--- @param time number
+function hilbish.interval(cb, time) end
 
 --- Changes the continued line prompt to `str`
-function hilbish.mlprompt() end
+--- @param str string
+function hilbish.mlprompt(str) end
 
 --- Prepends `dir` to $PATH
 function hilbish.prependPath() end
@@ -39,17 +47,22 @@ function hilbish.prependPath() end
 --- `%d` - Current working directory
 --- `%u` - Name of current user
 --- `%h` - Hostname of device
-function hilbish.prompt() end
+--- @param str string
+function hilbish.prompt(str) end
 
 --- Read input from the user, using Hilbish's line editor/input reader.
 --- This is a separate instance from the one Hilbish actually uses.
 --- Returns `input`, will be nil if ctrl + d is pressed, or an error occurs (which shouldn't happen)
-function hilbish.read() end
+--- @param prompt string
+function hilbish.read(prompt) end
 
 --- Runs `cmd` in Hilbish's sh interpreter.
-function hilbish.run() end
+--- @param cmd string
+function hilbish.run(cmd) end
 
 --- Runs the `cb` function after `time` in milliseconds
-function hilbish.timeout() end
+--- @param cb function
+--- @param time number
+function hilbish.timeout(cb, time) end
 
 return hilbish
