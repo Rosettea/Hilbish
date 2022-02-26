fd513b6ae307ebd9596fea1176c87ba474234c17
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-02-25 21:34:30 -0400

    fix: correct require paths on windows

--- vars_windows.go
+++ vars_windows.go
@@ -5,9 +5,9 @@ package main
 // String vars that are free to be changed at compile time
 var (
 	requirePaths = commonRequirePaths + `.. ';'
-	.. hilbish.user.config .. '\\Hilbish\\libs\\?\\init.lua;'
-	.. hilbish.user.config .. '\\Hilbish\\libs\\?\\?.lua;'
-	.. hilbish.user.config .. '\\Hilbish\\libs\\?.lua;'`
+	.. hilbish.userDir.config .. '\\Hilbish\\libs\\?\\init.lua;'
+	.. hilbish.userDir.config .. '\\Hilbish\\libs\\?\\?.lua;'
+	.. hilbish.userDir.config .. '\\Hilbish\\libs\\?.lua;'`
 	dataDir = "~\\Appdata\\Roaming\\Hilbish" // ~ and \ gonna cry?
 	preloadPath = dataDir + "\\preload.lua"
 	sampleConfPath = dataDir + "\\hilbishrc.lua" // Path to default/sample config
