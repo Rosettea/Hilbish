52379dbdd728419e115b556d7fda2500a8a2922d
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-02-28 18:53:58 -0400

    feat: support macos

---
+++ vars_darwin.go
@@ -0,0 +1,20 @@
+// +build darwin
+
+package main
+
+// String vars that are free to be changed at compile time
+var (
+	requirePaths = commonRequirePaths + `.. ';'
+	.. hilbish.dataDir .. '/libs/?/init.lua;'
+	.. hilbish.dataDir .. '/libs/?/?.lua;'` + macosUserPaths
+	macosUserPaths = `
+	.. hilbish.userDir.data     .. '/hilbish/libs/?/init.lua;'
+	.. hilbish.userDir.data	    .. '/hilbish/libs/?/?.lua;'
+	.. hilbish.userDir.data	    .. '/hilbish/libs/?.lua;'
+	.. hilbish.userDir.config	.. '/hilbish/?/init.lua;'
+	.. hilbish.userDir.config	.. '/hilbish/?/?.lua;'
+	.. hilbish.userDir.config	.. '/hilbish/?.lua'`
+	dataDir = "/usr/local/share/hilbish"
+	preloadPath = dataDir + "/preload.lua"
+	sampleConfPath = dataDir + "/.hilbishrc.lua" // Path to default/sample config
+)
