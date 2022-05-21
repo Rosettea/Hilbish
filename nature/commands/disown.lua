dde56cbb4fd088809249af63f8e7e8b1269de9f7
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-05-21 11:54:07 -0400

    feat: make disown command get last job if id isnt suppied as arg

--- nature/commands/disown.lua
+++ nature/commands/disown.lua
@@ -1,14 +1,25 @@
 local commander = require 'commander'
 
 commander.register('disown', function(args)
-	local id = tonumber(args[1])
-	if not id then
-		print 'invalid id for job'
+	if #hilbish.jobs.all() == 0 then
+		print 'disown: no current job'
 		return 1
+	end
+
+	local id
+	if #args < 0 then
+		id = tonumber(args[1])
+		if not id then
+			print 'disown: invalid id for job'
+			return 1
+		end
+	else
+		id = hilbish.jobs.last().id
 	end
 
 	local ok = pcall(hilbish.jobs.disown, id)
 	if not ok then
-		print 'job does not exist'
+		print 'disown: job does not exist'
+		return 2
 	end
 end)
