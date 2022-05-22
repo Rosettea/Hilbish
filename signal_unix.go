392cb66382c01c15d713642f82b044bcd62edb4f
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-05-21 20:53:02 -0400

    fix: handle sigterm and remove obsolete code

--- signal_unix.go
+++ signal_unix.go
@@ -10,20 +10,13 @@ import (
 
 func handleSignals() {
 	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGWINCH, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGQUIT)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGWINCH, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGQUIT)
 
 	for s := range c {
 		switch s {
-		case os.Interrupt:
-			hooks.Em.Emit("signal.sigint")
-			if !running && interactive {
-				lr.ClearInput()
-			}
-		case syscall.SIGWINCH:
-			hooks.Em.Emit("signal.resize")
-			if !running && interactive {
-				lr.Resize()
-			}
+		case os.Interrupt: hooks.Em.Emit("signal.sigint")
+		case syscall.SIGTERM: exit(0)
+		case syscall.SIGWINCH: hooks.Em.Emit("signal.resize")
 		case syscall.SIGUSR1: hooks.Em.Emit("signal.sigusr1")
 		case syscall.SIGUSR2: hooks.Em.Emit("signal.sigusr2")
 		}
