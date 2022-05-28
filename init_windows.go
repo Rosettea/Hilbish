e3c25586e42f770f85401440cb32cc6752fdfd39
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-05-28 09:07:28 -0400

    fix: broken ansi escape codes on windows cmd

---
+++ init_windows.go
@@ -0,0 +1,11 @@
+// +build windows
+
+package main
+
+import "golang.org/x/sys/windows"
+
+func init() {
+	var mode uint32
+	windows.GetConsoleMode(windows.Stdout, &mode)
+	windows.SetConsoleMode(windows.Stdout, mode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+}
