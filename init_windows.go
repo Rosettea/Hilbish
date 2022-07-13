dd9aa4b6ea58b9cd6dde5e4a178edb9184f68e76
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-07-13 15:04:18 -0400

    fix: enable vt input for windows

--- init_windows.go
+++ init_windows.go
@@ -7,5 +7,5 @@ import "golang.org/x/sys/windows"
 func init() {
 	var mode uint32
 	windows.GetConsoleMode(windows.Stdout, &mode)
-	windows.SetConsoleMode(windows.Stdout, mode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	windows.SetConsoleMode(windows.Stdout, mode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING | windows.ENABLE_VIRTUAL_TERMINAL_INPUT)
 }
