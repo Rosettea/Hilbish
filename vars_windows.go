e185a3268540c50681941115edc187aa3156760e
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-07-13 14:10:29 -0400

    fix: expand tilde in dataDir on windows

--- vars_windows.go
+++ vars_windows.go
@@ -2,13 +2,17 @@
 
 package main
 
+import (
+	"hilbish/util"
+)
+
 // String vars that are free to be changed at compile time
 var (
 	requirePaths = commonRequirePaths + `.. ';'
 	.. hilbish.userDir.config .. '\\Hilbish\\libs\\?\\init.lua;'
 	.. hilbish.userDir.config .. '\\Hilbish\\libs\\?\\?.lua;'
 	.. hilbish.userDir.config .. '\\Hilbish\\libs\\?.lua;'`
-	dataDir = "~\\Appdata\\Roaming\\Hilbish" // ~ and \ gonna cry?
+	dataDir = util.ExpandHome("~\\Appdata\\Roaming\\Hilbish") // ~ and \ gonna cry?
 	preloadPath = dataDir + "\\nature\\init.lua"
 	sampleConfPath = dataDir + "\\hilbishrc.lua" // Path to default/sample config
 	defaultConfDir = ""
