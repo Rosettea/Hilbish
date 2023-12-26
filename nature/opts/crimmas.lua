c1b78a8ea78e54003bacae82432f6520237bae70
sammyette <torchedsammy@gmail.com>
2023-12-26 10:13:35 -0400

    fix: remove debug print

--- nature/opts/crimmas.lua
+++ nature/opts/crimmas.lua
@@ -4,7 +4,6 @@ bait.catch('hilbish.init', function()
 	
 	if os.date '%m' == '12' and hilbish.interactive and hilbish.opts.crimmas then
 		local crimmas = math.random(1, 31)
-		print(crimmas)
 		if crimmas >= 25 and crimmas <= 29 then
 			print(lunacolors.format '🎄 {green}Merry {red}Christmas{reset} from your {green}favourite{reset} shell {red}(right?){reset} 🌺')
 		end
