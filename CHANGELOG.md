2bf037e51b6a951d74d7304374d8dd529ce15261
Nathan Helmig <nathanhelmig@pop-os.attlocal.net>
2024-07-20 19:21:04 -0500

    fix: added a few more tips related to hilbish features

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,11 @@
 # 🎀 Changelog
 
-## [2.3.0] - 2024-07-20
+## Unreleased
 ### Added
 - `nature.opts.tips` was added to randomly generate tips on start up.
 Follows the pattern of motd and greeting. Displays after greeting on start up.
+
+## [2.3.0] - 2024-07-20
 ### Added
 - `commander.registry` function to get all registered commanders.
 - `fs.pipe` function to get a pair of connected files (a pipe).
--- nature/opts/tips.lua
+++ nature/opts/tips.lua
@@ -11,9 +11,18 @@ hilbish.tips = {
 	"Join the discord and say hi! -> https://discord.gg/3PDdcQz",
 	"{green}hilbish.alias{reset} -> Sets an alias to another cmd",
 	"{green}hilbish.appendPath{reset} -> Appends the provided dir to the command path ($PATH)",
-	"{green}hilbish.appendPath{reset} -> Appends the provided dir to the command path ($PATH)",
+	"{green}hilbish.completions{reset} -> Are use to control suggestions when tab completing.",
+	"{green}hilbish.message{reset} -> Simple notification system which can be used by other plugins and parts of the shell to notify the user of various actions.",
 	[[
-	Add Lua-written commands with the commander module! 
+	{green}hilbish.opts{reset} -> Simple toggle or value options a user can set.
+		- EX: hilbish.opts.greeting = false, will cause the greeting message on start-up to not display.
+	]],
+	[[
+	{green}hilbish.runner{reset} -> The runner interface contains functions that allow the user to change how Hilbish interprets interactive input.
+		- The default runners can run shell script and Lua code.
+	]],
+	[[
+	Add Lua-written commands with the commander module!
 	Checkout the docs here -> https://rosettea.github.io/Hilbish/docs/api/commander/
 	]]
 }
