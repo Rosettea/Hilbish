e4df61f020f29c8d5178e5666f99064ecabe23a7
sammyette <torchedsammy@gmail.com>
2024-07-27 14:19:53 -0400

    fix(nature/tips): cleanup tips display

--- nature/opts/tips.lua
+++ nature/opts/tips.lua
@@ -1,34 +1,35 @@
 local bait = require 'bait'
 local lunacolors = require 'lunacolors'
 
-PREAMBLE = [[
-Getting Started: https://rosettea.github.io/Hilbish/docs/getting-started/
-{yellow}🛈 These tips can be disabled with hilbish.opts.tips = false{reset}
+local postamble = [[
+{yellow}These tips can be disabled with {reset}{invert} hilbish.opts.tips = false {reset}
 ]]
 
 hilbish.tips = {
-	"Join the discord and say hi! -> https://discord.gg/3PDdcQz",
-	"{green}hilbish.alias{reset} -> Sets an alias to another cmd",
-	"{green}hilbish.appendPath{reset} -> Appends the provided dir to the command path ($PATH)",
-	"{green}hilbish.completions{reset} -> Are use to control suggestions when tab completing.",
-	"{green}hilbish.message{reset} -> Simple notification system which can be used by other plugins and parts of the shell to notify the user of various actions.",
+	'Join the discord and say hi! {blue}https://discord.gg/3PDdcQz{reset}',
+	'{green}hilbish.alias{reset} interface manages shell aliases. See more detail by running {blue}doc api hilbish.alias.',
+	'{green}hilbish.appendPath(\'path\'){reset} -> Appends the provided dir to the command path ($PATH)',
+	'{green}hilbish.completions{reset} -> Used to control suggestions when tab completing.',
+	'{green}hilbish.message{reset} -> Simple notification system which can be used by other plugins and parts of the shell to notify the user of various actions.',
 	[[
-	{green}hilbish.opts{reset} -> Simple toggle or value options a user can set.
-		- EX: hilbish.opts.greeting = false, will cause the greeting message on start-up to not display.
-	]],
-	[[
-	{green}hilbish.runner{reset} -> The runner interface contains functions that allow the user to change how Hilbish interprets interactive input.
-		- The default runners can run shell script and Lua code.
-	]],
-	[[
-	Add Lua-written commands with the commander module!
-	Checkout the docs here -> https://rosettea.github.io/Hilbish/docs/api/commander/
-	]]
+{green}hilbish.opts{reset} -> Simple toggle or value options a user can set.
+You may disable the startup greeting by {invert}hilbish.opts.greeting = false{reset}
+]],
+[[
+{green}hilbish.runner{reset} -> The runner interface contains functions to
+manage how Hilbish interprets interactive input. The default runners can run
+shell script and Lua code!
+]],
+[[
+Add Lua-written commands with the commander module!
+Check the command {blue}doc api commander{reset} or the web docs:
+https://rosettea.github.io/Hilbish/docs/api/commander/
+]]
 }
 
 bait.catch('hilbish.init', function()
 	if hilbish.interactive and hilbish.opts.tips then
 		local idx = math.random(1, #hilbish.tips)
-		print(lunacolors.format(PREAMBLE .. "\nTip: " .. hilbish.tips[idx]))
+		print(lunacolors.format('{yellow}🛈 Tip:{reset} ' .. hilbish.tips[idx] .. '\n' .. postamble))
 	end
 end)
