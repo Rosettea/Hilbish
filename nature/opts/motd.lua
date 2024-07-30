137efe5c629f51e8613d3abc5203c98674f7cb7d
sammyette <torchedsammy@gmail.com>
2024-07-30 19:24:57 -0400

    fix: motd typo

--- nature/opts/motd.lua
+++ nature/opts/motd.lua
@@ -4,7 +4,7 @@ local lunacolors = require 'lunacolors'
 hilbish.motd = [[
 Wait ... {magenta}2.3{reset} is basically the same as {red}2.2?{reset}
 Erm.. {blue}Ctrl-C works for Commanders,{reset} {cyan}and the sh runner has some fixes.{reset}
-Just trust me bro, this is an imporant bug fix release. {red}- 🌺 sammyette{reset}
+Just trust me bro, this is an important bug fix release. {red}- 🌺 sammyette{reset}
 ]]
 
 bait.catch('hilbish.init', function()
